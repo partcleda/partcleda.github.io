807739cf2535ed9861e806da99030e7347f105b8
Vamshi Balanaga <vamshi@partcleda.com>
2025-03-25 21:54:43 -0700

    comment out ga func

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,24 +1,24 @@
 import { onCLS, onINP, onLCP } from 'web-vitals';
 
 
-function sendToGoogleAnalytics({ name, delta, value, id }) {
-  // Assumes the global `gtag()` function exists, see:
-  // https://developers.google.com/analytics/devguides/collection/ga4
-  gtag('event', name, {
-    // Built-in params:
-    value: delta, // Use `delta` so the value can be summed.
-    // Custom params:
-    metric_id: id, // Needed to aggregate events.
-    metric_value: value, // Optional.
-    metric_delta: delta, // Optional.
+// function sendToGoogleAnalytics({ name, delta, value, id }) {
+//   // Assumes the global `gtag()` function exists, see:
+//   // https://developers.google.com/analytics/devguides/collection/ga4
+//   gtag('event', name, {
+//     // Built-in params:
+//     value: delta, // Use `delta` so the value can be summed.
+//     // Custom params:
+//     metric_id: id, // Needed to aggregate events.
+//     metric_value: value, // Optional.
+//     metric_delta: delta, // Optional.
 
-    // OPTIONAL: any additional params or debug info here.
-    // See: https://web.dev/articles/debug-performance-in-the-field
-    // metric_rating: 'good' | 'needs-improvement' | 'poor',
-    // debug_info: '...',
-    // ...
-  });
-}
+//     // OPTIONAL: any additional params or debug info here.
+//     // See: https://web.dev/articles/debug-performance-in-the-field
+//     // metric_rating: 'good' | 'needs-improvement' | 'poor',
+//     // debug_info: '...',
+//     // ...
+//   });
+// }
 
 
 const reportWebVitals = onPerfEntry => {
